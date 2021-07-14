7645508e66a21d25f74fbbaaffbd4d1d15cd4f3f
Süha Kesikbaş <suhakesikbas@Suhas-MacBook-Pro.local>
2021-07-14 16:45:15 +0300

    İf Else örnekleri eklendi.

---
+++ 05-ControlStatement/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05_ControlStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın!");
+            }
+            else if (time < 11)
+            {
+                Console.WriteLine("İyi Günler!");
+            }
+            else
+            {
+                Console.WriteLine("İyi Geceler!");
+            }
+
+            //string sonuc = time <=18 ?"İyi Günler!":"İyi Geceler!";
+            string sonuc = time >= 6 && time < 11 ?"Günaydın": time <11 ?"İyi Günler!":"İyi Geceler!";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
