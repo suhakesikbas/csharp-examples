bc78c504bbbae7ab17e32f2821b89f2bd98bb889
Süha Kesikbaş <suha.kesikbas@outlook.com>
2021-07-21 16:55:24 +0300

    Metot örnkerli eklendi.

---
+++ 07-Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =5, b =8;
+            Console.WriteLine(Topla(a, b));
+
+            Metotlar metotlar = new Metotlar();
+            metotlar.EkranaYazdir("Lorem ipsum dolar sit amet");
+            int sonuc = metotlar.ToplaveArtir(ref a,ref b);
+            metotlar.EkranaYazdir(sonuc.ToString());
+            metotlar.EkranaYazdir((a+b).ToString());
+        }
+
+        static int Topla(int a, int b) => a + b;
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir(string mesaj) => Console.WriteLine(mesaj);
+
+        public int ToplaveArtir(ref int a,ref int b)=> ++a + ++b;
+    }
+}
