ad633e29a60d6a41fe09a7928c9642cce0eb174b
Süha Kesikbaş <suha.kesikbas@outlook.com>
2021-07-22 10:16:53 +0300

    Metot Overloading ve Out Parametre Kullanımı

--- 07-Methods/Program.cs
+++ 07-Methods/Program.cs
@@ -14,15 +14,38 @@ namespace _07_Methods
             int sonuc = metotlar.ToplaveArtir(ref a,ref b);
             metotlar.EkranaYazdir(sonuc.ToString());
             metotlar.EkranaYazdir((a+b).ToString());
+
+            //Out Parametreler
+            string sayi = "999";
+            bool sonuc2 = int.TryParse(sayi, out int result);
+            if (sonuc2)
+            {
+                Console.WriteLine("Başarılı.");
+                Console.WriteLine(result);
+            }else{
+                Console.WriteLine("Başarısız.");
+            }
+
+            Topla(a,b,out int toplamSonuc);
+            Console.WriteLine(toplamSonuc);
+
+            int ifade = 999;
+            metotlar.EkranaYazdir(ifade);
+            metotlar.EkranaYazdir("Lorem","ipsum");
+
         }
 
         static int Topla(int a, int b) => a + b;
+        static int Topla(int a, int b, out int toplam) => toplam = a + b;
     }
 
     class Metotlar
     {
         public void EkranaYazdir(string mesaj) => Console.WriteLine(mesaj);
+        public void EkranaYazdir(int sayi) => Console.WriteLine(sayi);
+        public void EkranaYazdir(string mesaj,string mesaj2) => Console.WriteLine(mesaj+" " +mesaj2);
 
         public int ToplaveArtir(ref int a,ref int b)=> ++a + ++b;
+        
     }
 }
