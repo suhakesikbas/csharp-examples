107a195fa91221f5a7b528217ef8a1cea5fe3e73
Süha Kesikbaş <suha.kesikbas@outlook.com>
2021-08-04 18:44:48 +0300

    Koleksiyonlar ödev 3 eklendi.

---
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sesliHarfler = new HashSet<char> { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+            Console.WriteLine("Bir cümle yazın");
+            string cumle = Console.ReadLine();
+            char[] bulunanHarfler = cumle.Where(c=>sesliHarfler.Contains(c)).ToArray();
+
+            Array.Sort(bulunanHarfler);
+            for (int i = 0; i < bulunanHarfler.Length; i++)
+                Console.WriteLine(bulunanHarfler[i]);
+
+
+        }
+    }
+}
